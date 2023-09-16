9f87340eca781970d8f95ac88ea8121f9b1ca51f
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-09-16 15:36:26 +0530

    trying to manuplate components!

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -1,10 +1,10 @@
-
+import Text from './txt'
 import Com from './comp'
 function App() {
   return (
     <>
        <Com/>      {/*can return only one component but use empty tag to return multiple components */}
-      <h1>Msking 007</h1>
+       <Text/>
     </>
   )
 }
---
+++ 01ViteReact/src/txt.jsx
@@ -0,0 +1,12 @@
+function Text(){
+    return(
+        <>
+            <div>
+                <img src="https://avatars.githubusercontent.com/u/95128635?v=4" alt="" height={100} width={100} />
+                <button>Don't Click Here</button>
+            </div>
+        </>
+    )
+}
+
+export default Text
