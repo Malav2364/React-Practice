0d29cfd9d2071972367cf2a4bbd81d35f44e376b
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-09-16 23:13:07 +0530

    custom render !!

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -5,14 +5,32 @@ import App from './App.jsx'
 function MyApp(){
     return(
         <div>
-            <h1>Custom React Component !</h1>
+            <h1>Custom React Component | Malav</h1>  {/*custom react component*/}
         </div>
     )
 }
 
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google !'
+// }
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target:'_blank'},
+    'click me to visit googly'
+)
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>
+        Visit Google
+    </a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-        <App />
-        <MyApp />
-    </>
+    reactElement        
 ) 
