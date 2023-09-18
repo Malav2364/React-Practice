102de89e348ac4f5283b2a07cda125067a5b7089
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-09-18 23:15:17 +0530

    some changes !!

--- currencyConversion/src/App.jsx
+++ currencyConversion/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-      <h1 className='bg-blue-800 text-center '>Hello World</h1>
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+    style={{
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+    }}>
+      
+
+    </div>
   )
 }
 
