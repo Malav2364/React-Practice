4676d56b7688dfbc497a3dcedaf62c3195ae1bca
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-09-18 16:21:19 +0530

    bg proj continue

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor]  = useState("olive")
   return (
-    <>
-     <h1 className='bg-black'>Hello World</h1>
-    </>
+    <div className='w-full h-screen duration-200'
+    style={{backgroundColor: color}}
+    ></div>
   )
 }
 
