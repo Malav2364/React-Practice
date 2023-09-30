fa952f0f744715be01275fe2370b0485658877d6
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-09-30 10:44:18 +0530

    practice !!!

--- useStateHooks/src/App.jsx
+++ useStateHooks/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [name, setName] =useState("Krishn")
+  const [age, setAge] = useState(18)
+
+  function onChange(e) {
+    setName(e.target.value)
+  }
+
+  function changeAge() {
+    setAge(age + 1)
+  }
+
+  return (  
+    <div className='h-screen w-screen duration-200 flex justify-center items-center flex-col'
+      style={{backgroundColor: 'black'}}
+      >
+      <div className='flex  items-center flex-col gap-10'>
+          <input type="text" 
+            value={name}
+            onChange={onChange}
+            className='rounded-md bg-blue-300 text-center'
+          />
+          <button className='bg-blue-300 p-2 rounded-xl' onClick={changeAge}>Increment Age</button>
+          <p className='text-white'>Hi, {name} your age is {age} Radhe Radhe!</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
