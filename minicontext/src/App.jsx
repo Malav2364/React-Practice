4347363ea7be5ea848b7e9f70581cff80093dbe2
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-10-01 11:08:01 +0530

    hmm error !!!

--- minicontext/src/App.jsx
+++ minicontext/src/App.jsx
@@ -1,12 +1,13 @@
-
-import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/userContextProvider'
 
 function App() {
 
   return (
     <UserContextProvider>
-      <h1>Malav Patel</h1>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
---
+++ minicontext/src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useState } from 'react'
+import userContext from '../context/userContext'
+function Login() {
+    const [username, setUsername] = useState(' ')
+    const [password, setPassword] = useState(' ')
+
+    const {setUser} = useContext(userContext)
+
+    const handleSubmit = () => {
+        e.preventDefault()
+        setUser({username, password})
+    }
+  return (
+    <div>
+        <h2>Login</h2>
+        <input type="text" placeholder='username' 
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        />
+        <input type="text" placeholder='password' 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
